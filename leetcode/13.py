b38f7001fa8924a04a47d52d3b1d1631cb6327c5
Minseop Jeong <hpmltsws@gmail.com>
2024-01-14 11:04:46 +0900

    https://leetcode.com/problems/roman-to-integer/description/

---
+++ leetcode/13.py
@@ -0,0 +1,30 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        intValue = 0
+
+        romanToIntMap = {
+            "I": 1,
+            "IV": 4,
+            "V": 5,
+            "IX": 9,
+            "X": 10,
+            "XL": 40,
+            "L": 50,
+            "XC": 90,
+            "C": 100,
+            "CD": 400,
+            "D": 500,
+            "CM": 900,
+            "M": 1000
+        }
+
+        i = 0
+        while i < len(s):
+            if i < len(s) - 1 and s[i:i + 2] in romanToIntMap:
+                intValue = intValue + romanToIntMap[s[i:i + 2]]
+                i = i + 2
+            else:
+                intValue = intValue + romanToIntMap[s[i]]
+                i = i + 1
+        
+        return intValue        
